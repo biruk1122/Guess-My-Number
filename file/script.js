5c4ead56cfa63cb284e313617a87635340f8e9f2
biruk1122 <birukfikadu29@gmail.com>
2025-07-11 17:56:05 +0300

    selecting, manipulating and handling click events

--- file/script.js
+++ file/script.js
@@ -1,7 +1,51 @@
-"use strict"
-document.querySelector(".message").textContent = "🎉correct number"
-document.querySelector(".number").textContent = "13"
-document.querySelector(".score").textContent = "10"
+// "use strict"
+// document.querySelector(".message").textContent = "🎉correct number"
+// document.querySelector(".number").textContent = "13"
+// document.querySelector(".score").textContent = "10"
 
-document.querySelector(".guess").value = 23
-console.log(document.querySelector(".guess").value)
+// document.querySelector(".guess").value = 23
+// console.log(document.querySelector(".guess").value)
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let score = 20
+
+document.querySelector(".number").textContent = secretNumber
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value)
+  console.log(guess, typeof guess)
+
+  //when there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "🤔 No number!"
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number"
+
+    document.querySelector("body").style.backgroundColor = "#60b374"
+
+    document.querySelector(".number").style.width = "30rem"
+
+    //when guess is to High
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High"
+      score--
+      document.querySelector(".score").textContent = score
+    } else {
+      document.querySelector(".message").textContent = "🧨 You have lost"
+      document.querySelector(".score").textContent = 0
+    }
+
+    //when guess is to low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too Low"
+      score--
+      document.querySelector(".score").textContent = score
+    } else {
+      document.querySelector(".message").textContent = "🧨 You have lost"
+      document.querySelector(".score").textContent = 0
+    }
+  }
+})
