b3f53677ae02c17a8cb87e4b2c8e2537ba9ca7a0
biruk1122 <birukfikadu29@gmail.com>
2025-07-12 10:43:00 +0300

    adding to the again button reset functionality and implementing highscore

--- file/script.js
+++ file/script.js
@@ -6,10 +6,10 @@
 // document.querySelector(".guess").value = 23
 // console.log(document.querySelector(".guess").value)
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
+let highScore = 0
 
-document.querySelector(".number").textContent = secretNumber
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value)
   console.log(guess, typeof guess)
@@ -21,10 +21,16 @@ document.querySelector(".check").addEventListener("click", function () {
     //when player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number"
+    document.querySelector(".number").textContent = secretNumber
 
     document.querySelector("body").style.backgroundColor = "#60b374"
 
     document.querySelector(".number").style.width = "30rem"
+
+    if (score > highScore) {
+      highScore = score
+      document.querySelector(".highscore").textContent = highScore
+    }
 
     //when guess is to High
   } else if (guess > secretNumber) {
@@ -48,4 +54,16 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0
     }
   }
+})
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20
+  secretNumber = Math.trunc(Math.random() * 20) + 1
+
+  document.querySelector(".message").textContent = "Start guessing..."
+  document.querySelector(".score").textContent = score
+  document.querySelector(".number").textContent = "?"
+  document.querySelector(".guess").value = ""
+  document.querySelector("body").style.backgroundColor = "#222"
+  document.querySelector(".number").style.width = "15rem"
 })
